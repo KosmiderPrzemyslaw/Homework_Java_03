bac46ab4748e9b3f65f5126ae9057de32043d338
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2021-01-08 06:06:23 +0100

    taksks done

---
+++ Homework_Java_03_Servlety/src/main/java/day1_tasks/Servlet4.java
@@ -0,0 +1,32 @@
+package day1_tasks;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+@WebServlet("/get4")
+public class Servlet4 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String[]> parameterMap = request.getParameterMap();
+
+        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()
+        ) {
+            String key = entry.getKey();
+            String[] value = entry.getValue();
+
+            String output = Arrays.toString(value);
+
+            response.getWriter().println(key + ": ");
+            response.getWriter().println(output + "\n");
+        }
+    }
+}
